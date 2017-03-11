f8e67a49fdd3be05cf89199a99236eefa131f5cc
JoeScho <joeschofield@live.co.uk>
2017-03-11 13:59:27 +0000

    Some designing, login functionality

--- www/js/Controllers.js
+++ www/js/Controllers.js
@@ -1,28 +1,8 @@
 angular.module('starter.controllers', ['ionic'])
 
-    .controller('LoginCtrl', function ($scope) {
-    })
-
-    .controller('RegisterCtrl', function ($scope) {
-    })
-
-    .controller('ViewPatientsCtrl', function ($scope) {
-    })
-
-    .controller('PatientDetailsCtrl', function ($scope) {
-
-    })
-
-    .controller('SettingsCtrl', function ($scope) {
-    })
-
-    .controller('ChangePasswordCtrl', function ($scope) {
-    })
-
-
-    .controller('CarerController', function ($scope, $rootScope, $http, $window, $state, $cordovaGeolocation, $timeout) {
-        $scope.getDetails = function (id) {
-            $http.get('http://patienttrackapiv2.azurewebsites.net/api/Carers/' + id)
+    .controller('LoginCtrl', function ($scope, $http, $rootScope, $window) {
+        $scope.getDetails = function () {
+            $http.get('http://patienttrackapiv2.azurewebsites.net/api/Carers/' + this.loginEmail + '/' + this.loginPwd)
                 .success(function (data, status, headers, config) {
                     console.log('data success');
                     console.log(data); // for browser console
@@ -30,11 +10,32 @@ angular.module('starter.controllers', ['ionic'])
                     $window.location.href = '#/ViewPatients';
                 })
                 .error(function (data, status, headers, config) {
+                    $scope.showLoginFail();
                     console.log('data error');
                 });
         };
+    })
 
-        $scope.viewPatient = function (index, $timeout) {
+    .controller('RegisterCtrl', function ($scope) {
+        $scope.getDetails = function () {
+            var data = {
+
+            }
+            $http.post('http://patienttrackapiv2.azurewebsites.net/api/Carers/', data)
+                .success(function (data, status, headers, config) {
+                    console.log('Registered successfully');
+                    $window.location.href = '#/Login';
+                })
+                .error(function (data, status, headers, config) {
+                    $scope.showLoginFail();
+                    console.log('Registration error');
+                });
+        };
+        registerCarer
+    })
+
+    .controller('ViewPatientsCtrl', function ($scope, $http, $rootScope, $window) {
+        $scope.viewPatient = function (index) {
             $window.location.href = '#/PatientDetails'
             angular.element(document).ready(function () {
                 $rootScope.selectedPatient = $rootScope.carers.Patients[index];
@@ -60,6 +61,58 @@ angular.module('starter.controllers', ['ionic'])
                 });
             });
         }
+    })
+
+    .controller('PatientDetailsCtrl', function ($scope) {
+    })
+
+    .controller('SettingsCtrl', function ($scope) {
+    })
+
+    .controller('ChangePasswordCtrl', function ($scope) {
+    })
+
+
+    .controller('CarerController', function ($scope, $rootScope, $http, $window) {
+        // $scope.getDetails = function (id) {
+        //     $http.get('http://patienttrackapiv2.azurewebsites.net/api/Carers/' + id)
+        //         .success(function (data, status, headers, config) {
+        //             console.log('data success');
+        //             console.log(data); // for browser console
+        //             $rootScope.carers = data; // for UI
+        //             $window.location.href = '#/ViewPatients';
+        //         })
+        //         .error(function (data, status, headers, config) {
+        //             console.log('data error');
+        //         });
+        // };
+
+        // $scope.viewPatient = function (index, $timeout) {
+        //     $window.location.href = '#/PatientDetails'
+        //     angular.element(document).ready(function () {
+        //         $rootScope.selectedPatient = $rootScope.carers.Patients[index];
+        //         var lat = $rootScope.carers.Patients[index].Locations[$rootScope.carers.Patients[index].Locations.length - 1].Latitude;
+        //         var long = $rootScope.carers.Patients[index].Locations[$rootScope.carers.Patients[index].Locations.length - 1].Longitude;
+        //         console.log("Lat is " + lat + ", Long is " + long);
+        //         var latLng = new google.maps.LatLng(lat, long);
+        //         var mapOptions = {
+        //             center: latLng,
+        //             zoom: 15,
+        //             mapTypeId: google.maps.MapTypeId.ROADMAP
+        //         };
+        //         $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        //
+        //         // Wait until the map is loaded
+        //         google.maps.event.addListenerOnce($scope.map, 'idle', function () {
+        //
+        //             var marker = new google.maps.Marker({
+        //                 map: $scope.map,
+        //                 animation: google.maps.Animation.DROP,
+        //                 position: latLng
+        //             });
+        //         });
+        //     });
+        // }
     })
 
     .controller('PopupCtrl', function ($scope, $ionicPopup, $timeout) {
@@ -167,6 +220,18 @@ angular.module('starter.controllers', ['ionic'])
 
             alertPopup.then(function (res) {
                 console.log('Username changed');
+            });
+        };
+
+        // An alert dialog
+        $scope.showLoginFail = function () {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Details not found',
+                subTitle: 'Please check your credentials'
+            });
+
+            alertPopup.then(function (res) {
+                console.log('Login attempt failed');
             });
         };
     });
